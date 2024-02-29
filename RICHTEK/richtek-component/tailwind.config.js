0ab266550adc5acbec6780dc3e5e50575bdaafa4
dev_Mart <martumbaocommude@gmail.com>
2024-02-29 18:07:17 +0800

    add cards component

--- RICHTEK/richtek-component/tailwind.config.js
+++ RICHTEK/richtek-component/tailwind.config.js
@@ -10,8 +10,15 @@ export default {
         montserrat: ['Montserrat', 'sans-serif'],
         roboto: ['Roboto', 'sans-serif'],
       },
+      backgroundImage: {
+        'radial-radient': 'radial-gradient(at center center, #ffffff 0%, #b2b2b2 100%)'
+      },
+      screens: {
+        'md': {
+          'max': '768px'
+        }
+      }
     },
   },
   plugins: [],
 }
-
