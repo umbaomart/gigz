ec8b0bf596f47b22294f7f0cc9dbd2f445f3d72f
dev_Mart <martumbaocommude@gmail.com>
2024-04-03 22:27:47 +0800

    add assets and infinite slider for suitecharm

---
+++ SUITE CHARM/dev_mart/assets/js/script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const slidesItemLength = document.querySelectorAll('.swiper-slide').length
+
+    // Swiper
+    const swiper = new Swiper('.swiper.chooseUsSlider', {
+        loop: true,
+        spaceBetween: 0,
+        centeredSlides: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        speed: 1000,
+        // Add responsive breakpoints
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 2,
+            },
+            // when window width is >= 480px
+            480: {
+                slidesPerView: 3,
+            },
+            // when window width is >= 640px
+            640: {
+                slidesPerView: 5,
+            },
+            768: {
+                slidesPerView: 6,
+            },
+            1024: {
+                slidesPerView: 7,
+            },
+            1200: {
+                slidesPerView: 8,
+            }
+        }
+    });
+
+
+})
